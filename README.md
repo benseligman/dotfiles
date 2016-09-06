bcfe06b2d40fe7d0c789283fb45580f74772b1c4
Ben Seligman <seligman.ben@gmail.com>
2016-09-05 22:28:30 -0400

    Add zsh/alias configuration

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Installation
 
+curl to install oh-my-zsh, then
+
 ```bash
 git clone git://github.com/benseligman/dotfiles ~/dotfiles
 cd dotfiles
--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ fi
 
 cwd=`pwd`
 echo "creating symlinks"
-for file in vimrc tmux.conf; do
+for file in vimrc tmux.conf zshrc aliasrc; do
   echo " * $file"
   ln -sf $cwd/$file $HOME/.$file
 done
